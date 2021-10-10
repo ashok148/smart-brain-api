05f95e097abcb6a07d9c5a41f1563ba3d5f52664
Ashok Kumar <ashokkumar56624@gmail.com>
2021-10-10 15:59:49 +0530

    first commit

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const bodyParser = require('body-parser');
 const bcrypt = require('bcrypt-nodejs');
 const cors = require('cors');
 const knex = require('knex');
+const port = process.env.PORT || 8000;
 
 const register = require('./controllers/register');
 const signin = require('./controllers/signin');
@@ -32,6 +33,6 @@ app.get('/profile/:id', (req, res) => { profile.handleProfileGet(req, res, db)})
 app.put('/image', (req, res) => { image.handleImage(req, res, db)})
 app.post('/imageurl', (req, res) => { image.handleApiCall(req, res)})
 
-app.listen(3000, ()=> {
-  console.log('app is running on port 3000');
+app.listen(port, ()=> {
+  console.log(`app is running on port ${port}`);
 })
